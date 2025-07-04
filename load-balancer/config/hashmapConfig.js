2d4186fbd74ca7b555fb720af3276192bb167051
Daryn Ongera <ongeradaryn@gmail.com>
2025-07-04 13:11:05 +0300

    updated load balancer config

--- load-balancer/config/hashmapConfig.js
+++ load-balancer/config/hashmapConfig.js
@@ -1,4 +1,4 @@
 export default {
-  numSlots: process.env.HASHMAP_SLOTS || 512, // Number of slots in the hash ring
+  numSlots: process.env.HASHMAP_SLOTS || 512, // Number of slots in the hash rin
   numVirtualServers: process.env.VIRTUAL_SERVER_COUNT || 9, // Number of virtual servers per physical server
 };
